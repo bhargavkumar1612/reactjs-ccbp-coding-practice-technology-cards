790eeaf0115ebbb366e96e1a2c65de2596b7016b
bhargavkumar1612 <bhargavkumar1612@gmail.com>
2021-04-11 10:53:40 +0000

    first commit

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,9 +6,11 @@ const Card = props => {
   const {title, description, imgUrl, className} = cardDeets
   return (
     <div className={`card ${className}`}>
-      <h1 className="title">{title}</h1>
-      <p className="description">{description}</p>
-      <img className="img" src={imgUrl} alt="img" />
+      <div>
+        <h1 className="title">{title}</h1>
+        <p className="description">{description}</p>
+        <img className="img" src={imgUrl} alt="img" />
+      </div>
     </div>
   )
 }
